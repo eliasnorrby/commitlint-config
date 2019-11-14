7b1889a4205d351b3568df35c586137716bbb8cd
Elias Norrby <elias.norrby@gmail.com>
2019-11-14 08:30:37 +0100

    test: update test to cover --no-install case

--- setup.test.sh
+++ setup.test.sh
@@ -4,32 +4,67 @@ set -exo pipefail;
 
 ORIG_DIR=$(pwd)
 function finish {
+  if [ ! $? -eq 0 ] ; then
+    echo "There are failing tests"
+  else
+    echo "All tests passed!"
+  fi
   cd "$ORIG_DIR"
 }
 trap finish EXIT
 
-cd "$(mktemp -d)"
-git init
-npm init -y
-npx $ORIG_DIR
-ls -a
+function setup {
+  local FLAG=$1
+  cd "$(mktemp -d)"
+  git init
+  npm init -y
+  npx $ORIG_DIR $FLAG
+  ls -a
+}
 
-CONFIG_FILE="commitlint.config.js"
-echo "'$CONFIG_FILE' should exist"
-[ -e "$CONFIG_FILE" ]
+function common_test {
+  CONFIG_FILE="commitlint.config.js"
+  echo "'$CONFIG_FILE' should exist"
+  [ -e "$CONFIG_FILE" ]
 
-MESSAGE_FILE=".gitmessage"
-echo "'$MESSAGE_FILE' should exist"
-[ -e "$MESSAGE_FILE" ]
+  MESSAGE_FILE=".gitmessage"
+  echo "'$MESSAGE_FILE' should exist"
+  [ -e "$MESSAGE_FILE" ]
 
-echo "contents of $MESSAGE_FILE"
-cat "$MESSAGE_FILE"
+  echo "contents of $MESSAGE_FILE"
+  cat "$MESSAGE_FILE"
 
-echo "'commit.template' should be set"
-git config commit.template
+  echo "'commit.template' should be set"
+  git config commit.template
 
-echo "'@eliasnorrby/commitlint-config should be installed"
-[ -d "node_modules/@eliasnorrby/commitlint-config" ]
+  echo "'@commitlint/cli' should be installed"
+  [ -d "node_modules/@commitlint/cli" ]
+}
 
-echo "'@commitlint/cli' should be installed"
-[ -d "node_modules/@commitlint/cli" ]
+function install_test {
+  setup
+
+  common_test
+
+  echo "'@eliasnorrby/commitlint-config should be installed"
+  [ -d "node_modules/@eliasnorrby/commitlint-config" ]
+}
+
+function no_install_test {
+  setup "--no-install"
+
+  common_test
+
+  echo "'@eliasnorrby/commitlint-config should not be installed"
+  [ ! -d "node_modules/@eliasnorrby/commitlint-config" ]
+}
+
+function help_test {
+  npx $ORIG_DIR --help | grep "Usage"
+}
+
+install_test
+
+# no_install_test
+
+help_test
