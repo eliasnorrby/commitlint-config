bd0c1076ba6a5b4bbe0172fb96df925a6d8d9df3
Elias Norrby <elias.norrby@gmail.com>
2019-12-19 10:33:42 +0100

    feat: use @eliasnorrby/log-util during setup

--- setup.js
+++ setup.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const path = require("path");
 const fs = require("fs");
-const chalk = require("chalk");
 const yargs = require("yargs");
 const hasYarn = require("has-yarn")();
+
+const { log } = require("@eliasnorrby/log-util");
 
 const pkgInstall = hasYarn ? "yarn add" : "npm install";
 const pkgInstallDev = `${pkgInstall} -D`;
@@ -24,21 +25,10 @@ yargs
 
 const argv = yargs.argv;
 
-// Set up logging methods
-const log = {
-  info: msg =>
-    console.log(`${chalk.bgGreen.black(" INFO ")} ${chalk.green(msg)}`),
-  warn: msg =>
-    console.log(`${chalk.bgYellow.black(" WARN ")} ${chalk.yellow(msg)}`),
-  skip: msg => console.log(`${chalk.bgGray(" SKIP ")} ${msg}`),
-  error: msg =>
-    console.log(`${chalk.bgRed.black(" ERROR ")} ${chalk.red(msg)}`),
-};
-
 const packageName = "@eliasnorrby/commitlint-config";
 
 if (!fs.existsSync("package.json")) {
-  log.error(
+  log.fail(
     "No package.json found in the current directory. Make sure you are in the project root. If no package.json exists yet, run `npm init` first.",
   );
   process.exit(1);
